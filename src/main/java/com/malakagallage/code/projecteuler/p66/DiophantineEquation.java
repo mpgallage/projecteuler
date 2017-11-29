558dcf808ffb62bdb175081af0d2cf2e3f54a6d3
maal <mpgallage@gmail.com>
2017-11-30 00:57:32 +0800

    adding big decimal solution to p66

--- src/main/java/com/malakagallage/code/projecteuler/p66/DiophantineEquation.java
+++ src/main/java/com/malakagallage/code/projecteuler/p66/DiophantineEquation.java
@@ -1,45 +1,48 @@
 package com.malakagallage.code.projecteuler.p66;
 
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import com.malakagallage.code.projecteuler.util.EulerUtils;
+import java.math.BigDecimal;
 
 /**
  * @author : maal (Malaka Gallage)
- * @since : 27/11/17
+ * @since : 29/11/17
  */
 public class DiophantineEquation {
 	
     public static void main(String[] args) {
-    	
 
-    	double max = 0;
+    	BigDecimal max = new BigDecimal(0);
     	long answer = 0;
     	
-    	for (double d = 2; d <= 1000; d++) {
-    		
+    	for (int d = 2; d <= 1000; d++) {
+
     		double sqrt = Math.sqrt(d);
+            BigDecimal bigSQRT = new BigDecimal(String.valueOf(Math.ceil(sqrt)));
     		if (sqrt == (long) sqrt) {
     			continue;
     		}
     		
-    		for (double x = Math.floor(sqrt); true; x++) {
+    		for (BigDecimal x = bigSQRT; true;) {
     			
-    			double y = Math.sqrt((x * x - 1) / d);
+    			double y = Math.sqrt(x.pow(2).subtract(BigDecimal.ONE).divide(new BigDecimal(d), 2, BigDecimal.ROUND_FLOOR).doubleValue());
     			
     			if ((long) y == y) {
     				System.out.println(x +"^2 - " + d + "*" + y + "^2 = 1");
     				
-    				if (x > max) {
+    				if (x.compareTo(max) == 1) {
     					max = x;
     					answer = (long) d;
     				}
     				break;
     			}
-    		}
+
+				if (x.compareTo(bigSQRT) == 0) {
+					x = new BigDecimal(d - 1);
+				} else if (x.add(BigDecimal.ONE).remainder(new BigDecimal(d)).compareTo(BigDecimal.ZERO) == 0) {
+					x = x.add(new BigDecimal(2));
+				} else {
+					x = x.add(new BigDecimal(d - 2));
+				}
+			}
     	}
     	
     	System.out.println(answer);
