69232f30af5abbc6b062af7824f2e9b48bff40ee
malaka.fm <malaka.fm@PC0KPFZT.ebiz.dsta>
2017-11-29 11:51:41 +0800

    fixing p66

--- src/main/java/com/malakagallage/code/projecteuler/p65/ConvergentsOfE.java
+++ src/main/java/com/malakagallage/code/projecteuler/p65/ConvergentsOfE.java
@@ -1,5 +1,6 @@
 package com.malakagallage.code.projecteuler.p65;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -14,32 +15,30 @@ public class ConvergentsOfE {
 	
     public static void main(String[] args) {
     	
-    	double[] a = new double[101];
-    	double[] x = new double[101];
-    	double[] y = new double[101];
+    	BigInteger[] a = new BigInteger[101];
+    	BigInteger[] x = new BigInteger[101];
+    	BigInteger[] y = new BigInteger[101];
     	
-    	a[1] = 2;
+    	a[1] = new BigInteger("2");
     	
     	for (int i = 2, j = 1; i < 101; i++) {
     	
-			a[i] = i  % 3 == 0 ? i * 2 / 3 : 1;
+			a[i] = i  % 3 == 0 ? new BigInteger(String.valueOf(i * 2 / 3)) : new BigInteger("1");
 		}
     	
     	
     	x[100] = a[100];
-    	y[100] = 1;
+    	y[100] = new BigInteger("1");
     	
     	for (int i = 99; i > 0; i--) {
         	
-			x[i] = x[i + 1] * a[i] + y[i+1];
+			x[i] = x[i + 1].multiply(new BigInteger(String.valueOf(a[i]))).add(y[i+1]);
 			y[i] = x[i + 1];
 		}
     	
-    	
-    	String[] arr = EulerUtils.doubleToString(x[1]).split("");
+    	String[] arr = x[1].toString().split("");
     	int sum = Arrays.stream(arr).mapToInt(Integer::parseInt).sum();
-    	
-    	System.out.println(x[1]);
+
     	System.out.println(sum);
 	}
 }
