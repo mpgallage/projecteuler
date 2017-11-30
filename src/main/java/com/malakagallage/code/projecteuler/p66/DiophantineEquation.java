f0ddaf9e2c4b6adb42933bbfb4fdeb9625870bb3
maal <mpgallage@gmail.com>
2017-12-01 01:28:48 +0800

    adding answers to p66

--- src/main/java/com/malakagallage/code/projecteuler/p66/DiophantineEquation.java
+++ src/main/java/com/malakagallage/code/projecteuler/p66/DiophantineEquation.java
@@ -1,6 +1,11 @@
 package com.malakagallage.code.projecteuler.p66;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.malakagallage.code.projecteuler.util.EulerUtils.doubleToString;
 
 /**
  * @author : maal (Malaka Gallage)
@@ -10,41 +15,92 @@ public class DiophantineEquation {
 	
     public static void main(String[] args) {
 
-    	BigDecimal max = new BigDecimal(0);
-    	long answer = 0;
-    	
-    	for (int d = 2; d <= 1000; d++) {
+        int answer = 0;
+        BigInteger max = BigInteger.ZERO;
 
-    		double sqrt = Math.sqrt(d);
-            BigDecimal bigSQRT = new BigDecimal(String.valueOf(Math.ceil(sqrt)));
-    		if (sqrt == (long) sqrt) {
-    			continue;
-    		}
-    		
-    		for (BigDecimal x = bigSQRT; true;) {
-    			
-    			double y = Math.sqrt(x.pow(2).subtract(BigDecimal.ONE).divide(new BigDecimal(d), 2, BigDecimal.ROUND_FLOOR).doubleValue());
-    			
-    			if ((long) y == y) {
-    				System.out.println(x +"^2 - " + d + "*" + y + "^2 = 1");
-    				
-    				if (x.compareTo(max) == 1) {
-    					max = x;
-    					answer = (long) d;
-    				}
-    				break;
-    			}
+        for (int d = 2; d <= 1000; d ++) {
 
-				if (x.compareTo(bigSQRT) == 0) {
-					x = new BigDecimal(d - 1);
-				} else if (x.add(BigDecimal.ONE).remainder(new BigDecimal(d)).compareTo(BigDecimal.ZERO) == 0) {
-					x = x.add(new BigDecimal(2));
-				} else {
-					x = x.add(new BigDecimal(d - 2));
-				}
-			}
-    	}
-    	
-    	System.out.println(answer);
-	}
+            double sqrt = Math.sqrt(d);
+
+            if (sqrt != (long)sqrt) {
+
+                List<Double> list = expand(d, Math.floor(sqrt), 1, 0, 0);
+                int l = list.size();
+
+                list.add(Math.floor(sqrt));
+                list.add(0d);
+
+                if (l % 2 == 0) {
+
+                    list.remove(0);
+                    Collections.reverse(list);
+
+                } else {
+
+                    Collections.reverse(list);
+                    for (int i = 2; i <= l; i++) {
+                        list.add(list.get(i));
+                    }
+                }
+
+                int size = list.size();
+                BigInteger[] x = new BigInteger[size];
+                BigInteger[] y = new BigInteger[size];
+
+                x[size - 1] = new BigInteger(doubleToString(list.get(size - 1)));
+                y[size - 1] = BigInteger.ONE;
+
+                for (int i = size - 2; i > 0; i--) {
+
+                    x[i] = x[i + 1].multiply(new BigInteger(doubleToString(list.get(i)))).add(y[i + 1]);
+                    y[i] = x[i + 1];
+                }
+
+                if (x[1].compareTo(max) == 1) {
+
+                    max = x[1];
+                    answer = d;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static List<Double> expand(double x, double a, double b, double startA, double startB) {		// b/(Math.sqrt(x) - a)
+
+        double decider = (x - a * a);
+
+        if (decider <= 0 || x <= 1 || a <= 0 || b <= 0) {
+            return null;
+        }
+
+        double start = Math.ceil(a * b / decider);
+        double end = Math.floor(b / (Math.sqrt(x) - a));
+
+        double nextA;
+        double nextB = decider / b;
+
+        for (double i = start; i <= end; i++) {
+
+            nextA = i * decider / b - a;
+
+            if (startA != 0 && startB != 0 && startA == nextA && startB == nextB) {
+                return new ArrayList<>();
+            }
+
+            if (startA == 0 || startB == 0) {
+                startA = nextA;
+                startB = nextB;
+            }
+
+            List<Double> list = expand(x, nextA, nextB, startA, startB);
+            if (list != null) {
+                list.add(i);
+                return list;
+            }
+        }
+
+        return null;
+    }
 }
